93220a681eee98d1c617009d57d01c876ae79727
GunjeetKaur2511 <155669885+GunjeetKaur2511@users.noreply.github.com>
2025-06-28 18:44:24 +0530

    Update ShopContext.jsx

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -18,12 +18,12 @@ const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
     useEffect(()=>{
-        fetch('http://localhost:4000/allproducts')
+        fetch(`${process.env.REACT_APP_API_URL}/api/allproducts`)
         .then((response)=>response.json())
         .then((data)=>setAll_Product(data))
 
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/getcart',{
+            fetch(`${process.env.REACT_APP_API_URL}/api/getcart`,{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
@@ -43,7 +43,7 @@ const ShopContextProvider = (props) => {
     const addToCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/addtocart',{
+            fetch(`${process.env.REACT_APP_API_URL}/api/addtocart`,{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
@@ -60,7 +60,7 @@ const ShopContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/removefromcart',{
+            fetch(`${process.env.REACT_APP_API_URL}/api/removefromcart`,{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
