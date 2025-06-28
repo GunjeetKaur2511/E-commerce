e346897fbf769aac818973ff6032a85f15d1d0e3
GunjeetKaur2511 <155669885+GunjeetKaur2511@users.noreply.github.com>
2025-06-28 14:17:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use('/images', express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url:`https://e-commerce-backend-m42q.onrender.com/images/${req.file.filename}`
     })
 })
 
